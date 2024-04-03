9e5f39a912fa34a967c54d50db9cf576fc8bf6a8
kui34 <kui34@ENIU>
2024-04-03 22:05:46 +0200

    Add project files.

---
+++ multiplication_table/Program.cs
@@ -0,0 +1,32 @@
+﻿
+class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] table = new int[10, 10];
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    table[i, j] = (i + 1) * (j + 1);
+                }
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(table[i, j] + " ");
+
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+
+        }
+ }
+
+
+
